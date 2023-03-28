a47438a0d65593ebf2266198768168b7f9a67327
ismail <ismailsaghraoui@gmail.com>
2023-03-29 01:42:49 +0200

    center walida

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Hello world</h1>
+      <div style={{ textAlign: "center" }}>
+        <h1>Lwalida</h1>
+      </div>
     </div>
   );
 }
